1ede2dd89fc94f1162d84e13bd4a0f49ad429f81
Al5237168 <al5237168@gmail.com>
2023-12-20 23:32:35 +0500

    Initial commit

---
+++ frontend/src/UpdateStudent.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+function UpdateStudent() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const {id} = useParams();
+    const navigate = useNavigate();
+
+    function handleSubmit(event){
+        event.preventDefault();
+        console.log("ID:", id); // Add this line to log the ID
+        axios.put('http://localhost:8081/update/'+id, {name, email})
+        .then(res=> {
+            console.log("Response Data:", res.data); // Log the response data
+        navigate('/')
+        }).catch(err => console.log("axios_error_update",err));
+    }
+
+    return(
+        <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
+          <div className='w-50 bg-white rounded p-3'>
+            <form onSubmit={handleSubmit}>
+                <h2>Update Students</h2>
+                <div className="mb-2">
+                    <label htmlFor="">Name</label>
+                    <input type="text" placeholder="Enter Name" className="form-control" 
+                        onChange={e => setName(e.target.value)}
+                    />
+                </div>
+                <div className="mb-2">
+                    <label htmlFor="">Email</label>
+                    <input type="email" placeholder="Enter email" className="form-control" 
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                </div>
+                <button className="btn btn-success">Update</button>
+            </form>
+          </div>
+        </div>
+    )
+}
+
+export default UpdateStudent
